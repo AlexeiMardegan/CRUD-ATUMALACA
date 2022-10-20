606512fb92b8b4357de2393e4617bed54217755b
Alexei Mardegan <76541124+AlexeiMardegan@users.noreply.github.com>
2022-10-20 17:25:40 -0300

    Add files via upload
    Crud feito na aula com o professor André.

---
+++ exibe.php
@@ -0,0 +1,21 @@
+<?php
+include("conecta.php");
+$sql_ultima_temp = "SELECT * FROM 'controlador' ORDER BY 'controlador'.'id' DESC limit 1"
+$sql_media = "SELECT AVG (temperatura) FROM 'controlador'";
+$sql_maxima = "SELECT MAX (temperatura) FROM 'controlador'";
+$sql_minina =  "SELECT MIN (temperatura) FROM 'controlador'";
+
+$consulta = mysqli_query($conn,$sql_ultima_temp);
+$resultado = mysqli_fetch_array($consulta);
+echo "<b>".$resultado["$temperatura"]."</b>";
+$consulta2 = mysqli_query($conn,$sql_media);
+$resultado2 = mysqli_fetch_array($consulta2);
+echo "<b>".$resultado2["$temperatura"]."</b>";
+$consulta = mysqli_query($conn,$sql_minima);
+$resultado = mysqli_fetch_array($consulta);
+echo "<b>".$resultado["$temperatura"]."</b>";
+$consulta = mysqli_query($conn,$sql_maxima);
+$resultado = mysqli_fetch_array($consulta);
+echo "<b>".$resultado["$temperatura"]."</b>";
+
+?>
---
+++ registra.php
@@ -0,0 +1,19 @@
+<?php
+include ("conecta.php"); // conecta no DB
+$temperatura = $_GET['temp']; // busca temperatura via GET (na URL, o nome que aparece em cima do site) 
+
+
+$sql = "INSERT INTO `controlador` (`temperatura`) VALUES ('$temperatura')"; // comando SQL
+
+$insere = mysqli_query($conn,$sql);
+
+if ($insere){
+	echo "sucessoooooooooooooooooooooooooooooooooooooooooooooooo";
+}
+else{
+	echo "errou burrão babaca otario burro panaca lixo podre ruim cagão melhore";
+}
+
+
+
+?>
